82cac794192533bd42fd281de8a08e94909f89a4
I4PROINFO\anmak <andrew.mak@i4pro.com.br>
2018-09-05 18:59:00 -0300

    testar conexao

--- api/server.js
+++ api/server.js
@@ -4,3 +4,7 @@ const bodyParser = require('body-parser');
 const port = 3000;
 const sql = require('mssql');
 const connStr = "Server=xxx;Database=xxx;User Id=xx;Password=xxxxx;";
+
+sql.connect(connStr)
+   .then(conn => global.conn = conn)
+   .catch(err => console.log(err));
